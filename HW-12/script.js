b2cedaec7a3e8ef009a2e74644dc48dbc1a647f8
Vlad Arkhipov <arkhipovvladyslav@gmail.com>
2023-04-03 21:14:07 +0200

    Some changes are added

--- HW-12/script.js
+++ HW-12/script.js
@@ -1,13 +1,23 @@
 let arr = [];
+let arrOfNum = [];
+let arrOfStr = [];
 let arrLength = prompt("Array length is");
 if (arrLength === null || isNaN(arrLength)) {
   alert("Window is canceled");
 } else {
   arr.length = Number(arrLength);
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = prompt("Enter the element of array:");
+    let elem = prompt("Enter the element of array:");
+
+    if (!isNaN(elem)) {
+      arrOfNum.push(Number(elem));
+    } else {
+      arrOfStr.push(elem);
+    }
   }
-  arr.sort();
+  arrOfNum.sort((a, b) => a - b);
+  arrOfStr.sort();
+  arr = [...arrOfNum, ...arrOfStr];
   arr.splice(1, 3);
   alert(arr);
 }
