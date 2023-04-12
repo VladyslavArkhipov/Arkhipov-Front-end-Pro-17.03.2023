2111e32e28fe2a952088b2505b6311092fdcf0c7
VladyslavArkhipov <128177247+VladyslavArkhipov@users.noreply.github.com>
2023-04-12 13:40:54 +0200

    Add files via upload

---
+++ HW-18/index.js
@@ -0,0 +1,8 @@
+function pow(num, degree) {
+  return (x = num ** degree);
+}
+console.log(pow(2, 2));
+console.log(pow(3, 3));
+console.log(pow(4, 4));
+console.log(pow(5, 2));
+console.log(pow(6, 3));
