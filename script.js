77368f46e992d3e6cbf2aa587ce5f2a58a0dc2b8
Vlad Arkhipov <arkhipovvladyslav@gmail.com>
2023-04-23 15:32:20 +0200

    New hw

--- script.js
+++ script.js
@@ -1,65 +1,61 @@
-class Human {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
-  humanProfile() {
-    return `Name: ${this.name} | Age: ${this.age}`;
-  }
-}
-
-class Auto {
-  constructor(brand, model, year, number) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.number = number;
-  }
-
-  owner = `Empty`;
-
-  addOwner(human) {
-    if (human.age < 18) {
-      return console.log("This human is to young");
+class SuperMath {
+  check(obj) {
+    let decision = confirm(`Do you want to make this aktion?
+  ${obj.X} ${obj.znak} ${obj.Y}`);
+    if (decision === true) {
+      if (obj.znak === "+") {
+        return this.__proto__.plus(obj);
+      } else if (obj.znak === "-") {
+        return this.__proto__.minus(obj);
+      } else if (obj.znak === "*") {
+        return this.__proto__.umnozhit(obj);
+      } else if (obj.znak === "/") {
+        return this.__proto__.podelit(obj);
+      } else if (obj.znak === "%") {
+        return this.__proto__.procent(obj);
+      } else {
+        return this.__proto__.unknownZnak();
+      }
     } else {
-      return (this.owner = human);
+      return this.input();
     }
   }
 
-  autoInfo() {
-    if (this.owner === `Empty`) {
-      return console.log(
-        `Brand: ${this.brand} | Model: ${this.model} | Year: ${this.year} | Number: ${this.number} | Owner: ${this.owner}`
-      );
-    } else {
-      return console.log(
-        `Brand: ${this.brand} | Model: ${this.model} | Year: ${this.year} | Number: ${this.number} | Owner: ` +
-          this.owner.humanProfile()
-      );
-    }
+  input() {
+    let newAction = {
+      X: +prompt("Enter the first number:"),
+      Y: +prompt("Enter the second number:"),
+      znak: prompt("Enter the znak:"),
+    };
+    return this.check(newAction);
   }
 }
 
-const human1 = new Human("Vlad", 23);
-console.log(human1);
-console.log(human1.humanProfile());
+class Znak {
+  result;
+  plus(obj) {
+    return console.log((this.result = obj.X + obj.Y));
+  }
+  minus(obj) {
+    return console.log((this.result = obj.X - obj.Y));
+  }
+  umnozhit(obj) {
+    return console.log((this.result = obj.X * obj.Y));
+  }
+  podelit(obj) {
+    return console.log((this.result = obj.X / obj.Y));
+  }
+  procent(obj) {
+    return console.log((this.result = obj.X % obj.Y));
+  }
+  unknownZnak() {
+    return console.log("Your znak is unknown");
+  }
+}
 
-const auto1 = new Auto("Honda", "Civic", "2005", "BH5503BX");
-console.log(auto1);
+SuperMath.prototype.__proto__ = Znak.prototype;
 
-auto1.addOwner(human1);
-console.log(auto1);
+let obj = { X: 12, Y: 3, znak: "+" };
 
-auto1.autoInfo();
-
-const human2 = new Human("Denis", 14);
-console.log(human2);
-console.log(human2.humanProfile());
-
-const auto2 = new Auto("Daewoo", "Lanos", "2000", "BH3412AK");
-console.log(auto2);
-
-auto2.addOwner(human2);
-console.log(auto2);
-
-auto2.autoInfo();
+p = new SuperMath();
+p.check(obj);
