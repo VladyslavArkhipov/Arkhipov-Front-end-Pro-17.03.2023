dbf949b2d568c0fcd8be5d786e9c31c8031e5e8b
Vlad Arkhipov <arkhipovvladyslav@gmail.com>
2023-04-28 22:53:26 +0200

    new hw

---
+++ script.js
@@ -0,0 +1,20 @@
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
+const btn = document.querySelector("#btn");
+
+let url;
+/* form.action = "https://google.com"; */
+
+input.addEventListener("input", () => {
+  console.log(input.value);
+});
+
+btn.addEventListener("click", (e) => {
+  if (
+    input.value.slice(0, 8) !== "https://" ||
+    input.value.slice(0, 7) !== "http://"
+  ) {
+    url = "https://" + input.value;
+    form.action = url;
+  }
+});
