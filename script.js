0dacc6e7ba8a4a465f8a54f5e68f496f42cc67b9
Vlad Arkhipov <arkhipovvladyslav@gmail.com>
2023-04-28 22:55:41 +0200

    changes

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
 const btn = document.querySelector("#btn");
+const https = "https://";
+const http = "http://";
 
 let url;
 
 btn.addEventListener("click", () => {
-  if (
-    input.value.slice(0, 8) !== "https://" ||
-    input.value.slice(0, 7) !== "http://"
-  ) {
+  if (input.value.slice(0, 8) !== https || input.value.slice(0, 7) !== http) {
     url = "https://" + input.value;
     form.action = url;
   }
