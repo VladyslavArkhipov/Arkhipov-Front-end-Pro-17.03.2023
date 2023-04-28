08fc0ba517dd28409f0a587c0674e3f3e978bbb8
Vlad Arkhipov <arkhipovvladyslav@gmail.com>
2023-04-28 22:58:11 +0200

    changes

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const container = document.querySelector(".container");
 const img = document.createElement("img");
-container.appendChild(img);
-img.src = `img/${getRandomValue()}.jpg`;
+
+function randomImage() {
+  container.appendChild(img);
+  img.src = `img/${getRandomValue()}.jpg`;
+}
 
 function getRandomValue() {
   return Math.floor(Math.random() * (10 - 1) + 1);
 }
+
+randomImage();
