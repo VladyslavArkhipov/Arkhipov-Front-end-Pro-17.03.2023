af918d988a54c27135bae7bcd0f96d842b825fec
Vlad Arkhipov <arkhipovvladyslav@gmail.com>
2023-04-28 20:04:52 +0200

    new hw

--- script.js
+++ script.js
@@ -1,82 +1,10 @@
-class Hamburger {
-  constructor(burger, stuffing) {
-    this.burger = burger;
-    this.stuffing = stuffing;
-  }
-  topping = {
-    price: 0,
-    kal: 0,
-  };
-  static SIZE_SMALL = {
-    price: 50,
-    kal: 20,
-  };
-  static SIZE_LARGE = {
-    price: 100,
-    kal: 40,
-  };
-  static STUFFING_CHEESE = {
-    price: 10,
-    kal: 20,
-  };
-  static STUFFING_SALAD = {
-    price: 20,
-    kal: 5,
-  };
-  static STUFFING_POTATO = {
-    price: 15,
-    kal: 10,
-  };
-  static TOPPING_MAYO = {
-    price: 20,
-    kal: 5,
-  };
-  static TOPPING_SAUCE = {
-    price: 15,
-    kal: 0,
-  };
+const textarea = document.querySelector(".textarrea");
+const ghostDiv = document.querySelector(".ghost_div");
 
-  addTopping(typeOfTopping) {
-    this.topping.price += typeOfTopping.price;
-    this.topping.kal += typeOfTopping.kal;
+textarea.onfocus = function () {
+  ghostDiv.className = "ghost_div visible_div";
+};
 
-    return this.topping;
-  }
-
-  calulate() {
-    let result;
-    if (this.stuffing != undefined) {
-      return (result = this.burger.kal + this.stuffing.kal + this.topping.kal);
-    } else {
-      return (result = this.burger.kal + this.topping.kal);
-    }
-  }
-
-  calulatePrice() {
-    let result;
-    if (this.stuffing != undefined) {
-      return (result =
-        this.burger.price + this.stuffing.price + this.topping.price);
-    } else {
-      return (result = this.burger.price + this.topping.price);
-    }
-  }
-}
-
-let p = new Hamburger(Hamburger.SIZE_SMALL);
-console.log(p);
-
-let a = new Hamburger(Hamburger.SIZE_LARGE, Hamburger.STUFFING_CHEESE);
-console.log(a);
-
-p.addTopping(Hamburger.TOPPING_MAYO);
-console.log(p);
-
-console.log("Calories:" + p.calulate());
-console.log("Price:" + p.calulatePrice());
-
-p.addTopping(Hamburger.TOPPING_SAUCE);
-console.log(p);
-
-console.log("Calories:" + p.calulate());
-console.log("Price:" + p.calulatePrice());
+textarea.onblur = function () {
+  ghostDiv.className = "ghost_div";
+};
