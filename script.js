a6b2cfde0307bac2bfbed9b078564c77f1da60a6
Vlad <arkhipovvladyslav@gmail.com>
2023-05-20 00:03:53 +0200

    new hw

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const form = document.querySelector("form"); // Нахожу форму
-const weatherWrapper = document.querySelector(".weather");
+const weatherWrapper = document.querySelector(".weather"); //Нахожу обертку для блока с погодой
 let city; //Переменная для названия города
 
 form.addEventListener("submit", showPost);
@@ -8,32 +8,43 @@ function showPost(e) {
   e.preventDefault(); // Отменяю стандартное поведение кнопки
   const formData = new FormData(form); // Создаю объект типа ФОРМДАТА для получения ввода инпута, который соответстует номеру поста
   city = formData.get("city");
-
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=afc6678d7fdc37aceff381a76878749d&lang=ua`
   )
     .then((response) => response.json())
-    .then(postCreating) // Если инпут в пределах допустимого значения то делаю запрос на сервер для получения обьекта с данными поста, а затем вывожу пост на экран и в случае ошибки вывожу через алерт сообщение с ошибкой
-    .catch((e) => alert("Ошибка соединения с сервером / " + e.message));
+    .then(weatherCreating) // Получаю данные с сервера и вывожу блок с погодой
+    .catch(() => alert("Такого города не существует. Попробуйте другой"));
 }
 
-function postCreating(json) {
-  const currentWeather = json.weather[0];
-  console.log(currentWeather);
+function weatherCreating(json) {
+  const currentWeather = json.weather[0]; //Инициализирую три переменные для более простого доступа к ним в объекте с сервера
+  const mainData = json.main;
+  const windData = json.wind;
   weatherWrapper.innerHTML = `
   <div class="container">
       <div class="col">
         <div class="icon">
-          <img  src="http://openweathermap.org/img/w/04n.png" alt="image">
-          <p>pressure</p>
-          <p>humidity</p>
-          <p>speed and deg</p>
+          <img  src="http://openweathermap.org/img/w/${
+            currentWeather.icon
+          }.png" alt="image">
+          <p>Тиск: ${mainData.pressure} мм рт. ст.</p>
+          <p>Вологiсть: ${mainData.humidity}%</p>
+          <p>Швидкiсть вiтру: ${windData.speed} м/с </p>
+          <p>Напрям вiтру: ${windDescription(windData.deg)}</p> 
         </div>
       </div>
       <div class="col">
-        <h1>temperature</h1>
-        <div class="description">
-          <p>${currentWeather.description}</p>
+      <h2>${validString(currentWeather.description)}</h2>
+        <div class='temperature'>
+          <p>Мiнiмальна температура: ${temperatureConvertation(
+            mainData.temp_min
+          )}ºC</p>
+          <p>Максимальна температура: ${temperatureConvertation(
+            mainData.temp_max
+          )}ºC</p>
+          <p>Вiдчувається як: ${temperatureConvertation(
+            mainData.feels_like
+          )}ºC</p>
         </div>
       </div>
     </div>
@@ -41,9 +52,50 @@ function postCreating(json) {
   console.log(json);
 }
 
-/* fetch(
-  "https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=afc6678d7fdc37aceff381a76878749d"
-)
-  .then((response) => response.json())
-  .then((json) => console.log(json))
-  .catch((e) => console.log(e.message)); */
+function validString(str) {
+  return (str = str[0].toUpperCase() + str.substring(1));
+}
+
+function temperatureConvertation(currentTemperatureInKelvin) {
+  const degree = Math.floor(currentTemperatureInKelvin - 273.15);
+  return degree;
+}
+
+function windDescription(windDegree) {
+  if (
+    (windDegree >= 0 && windDegree <= 10) ||
+    (windDegree >= 350 && windDegree <= 360)
+  ) {
+    return `Пiвнiчний`;
+  } else if (windDegree >= 20 && windDegree <= 30) {
+    return `Пiвнiчний/Пiвнiчно-схiдний`;
+  } else if (windDegree >= 40 && windDegree <= 50) {
+    return `Пiвнiчно-схiдний`;
+  } else if (windDegree >= 60 && windDegree <= 70) {
+    return `Cхiдний/Пiвнiчно-схiдний`;
+  } else if (windDegree >= 80 && windDegree <= 100) {
+    return `Cхiдний`;
+  } else if (windDegree >= 110 && windDegree <= 120) {
+    return `Cхiдний/Пiвденно-схiдний`;
+  } else if (windDegree >= 130 && windDegree <= 140) {
+    return `Пiвденно-схiдний`;
+  } else if (windDegree >= 150 && windDegree <= 160) {
+    return `Пiвденний/Пiвденно-схiдний`;
+  } else if (windDegree >= 170 && windDegree <= 190) {
+    return `Пiвденний`;
+  } else if (windDegree >= 200 && windDegree <= 210) {
+    return `Пiвденний/Пiвденно-захiдний`;
+  } else if (windDegree >= 220 && windDegree <= 230) {
+    return `Пiвденно-захiдний`;
+  } else if (windDegree >= 240 && windDegree <= 250) {
+    return `Захiдний/Пiвденно-захiдний`;
+  } else if (windDegree >= 260 && windDegree <= 280) {
+    return `Захiдний`;
+  } else if (windDegree >= 290 && windDegree <= 300) {
+    return `Захiдний/Пiвнiчно-захiдний`;
+  } else if (windDegree >= 310 && windDegree <= 320) {
+    return `Пiвнiчно-захiдний`;
+  } else if (windDegree >= 330 && windDegree <= 340) {
+    return `Пiвнiчний/Пiвнiчно-захiдний`;
+  }
+}
