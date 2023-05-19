249effb078a7b32a57685d2747e81fffc74b6deb
Vlad <arkhipovvladyslav@gmail.com>
2023-05-19 22:49:02 +0200

    hw-35

--- script.js
+++ script.js
@@ -1,58 +1,49 @@
 const form = document.querySelector("form"); // Нахожу форму
-const postWrapper = document.querySelector(".post"); // Нахожу обертку для поста
-const commentsWrapper = document.querySelector(".comments"); // Нахожу обертку для комментов
-let postId; // Переменная в которой будет храниться введенный пользователем номер поста
-const minPostId = 1; // Минимальный номер поста
-const maxPostId = 100; // Максимальный номер поста
+const weatherWrapper = document.querySelector(".weather");
+let city; //Переменная для названия города
 
 form.addEventListener("submit", showPost);
 
 function showPost(e) {
   e.preventDefault(); // Отменяю стандартное поведение кнопки
   const formData = new FormData(form); // Создаю объект типа ФОРМДАТА для получения ввода инпута, который соответстует номеру поста
-  postId = formData.get("postId");
-  if (postId >= minPostId && postId <= maxPostId) {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-      .then((response) => response.json())
-      .then(postCreating) // Если инпут в пределах допустимого значения то делаю запрос на сервер для получения обьекта с данными поста, а затем вывожу пост на экран и в случае ошибки вывожу через алерт сообщение с ошибкой
-      .catch((e) => alert("Ошибка соединения с сервером / " + e.message));
-  } else {
-    alert("Номер поста должен быть от 1 до 100");
-  }
-  commentsWrapper.innerHTML = ""; // Очищаю блок с комментариями на тот случай если там были комменты от предыдущего поста
-}
+  city = formData.get("city");
 
-function postCreating(json) {
-  postWrapper.innerHTML = `
-    <p class="post_user_id">user: ${json.userId}</p>
-    <h1 class="post_title">${json.title}</h1>
-    <p class="post_text">${json.body} </p>
-    <button class='show_comments'>Show comments</button> 
-`; // Из полученного после запроса объекта я получаю по ключам необходимые значения и подставляю
-  postWrapper.style.marginBottom = "50px";
-  postWrapper.style.padding = "20px";
-  const cmntBtn = document.querySelector(".show_comments"); // Нахожу кнопку для показа комментов
-  cmntBtn.addEventListener("click", showComments); // При клике показываю комментарии
-}
-
-function showComments() {
-  commentsWrapper.innerHTML = ""; // Очищаю блок с комментариями на тот случай если там были комменты от предыдущего поста
-  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=afc6678d7fdc37aceff381a76878749d&lang=ua`
+  )
     .then((response) => response.json())
-    .then(commentsCreating) // Делаю запрос на сервер чтобы получить массив комментариев и в случае ошибки вывожу ошибку через алерт
+    .then(postCreating) // Если инпут в пределах допустимого значения то делаю запрос на сервер для получения обьекта с данными поста, а затем вывожу пост на экран и в случае ошибки вывожу через алерт сообщение с ошибкой
     .catch((e) => alert("Ошибка соединения с сервером / " + e.message));
 }
 
-function commentsCreating(json) {
-  json.forEach((e) => {
-    let div = document.createElement("div");
-    div.className = "comment";
-    div.style.padding = "20px";
-    div.innerHTML = `
-    <p class="comment_user_name"><b>Name: </b>${e.name}</p>
-      <p class="comment_user_email"><span class="email">E-Mail: </span>${e.email}</p>
-      <p class="comment_text"><b>Comment: </b>${e.body}</p>
-    `;
-    commentsWrapper.append(div); // Прохожусь по массиву обьектов полученных от сервера и подставляю каждое значение туда куда нужно и создаю блок с комментом
-  });
+function postCreating(json) {
+  const currentWeather = json.weather[0];
+  console.log(currentWeather);
+  weatherWrapper.innerHTML = `
+  <div class="container">
+      <div class="col">
+        <div class="icon">
+          <img  src="http://openweathermap.org/img/w/04n.png" alt="image">
+          <p>pressure</p>
+          <p>humidity</p>
+          <p>speed and deg</p>
+        </div>
+      </div>
+      <div class="col">
+        <h1>temperature</h1>
+        <div class="description">
+          <p>${currentWeather.description}</p>
+        </div>
+      </div>
+    </div>
+  `;
+  console.log(json);
 }
+
+/* fetch(
+  "https://api.openweathermap.org/data/2.5/weather?q=Lviv&appid=afc6678d7fdc37aceff381a76878749d"
+)
+  .then((response) => response.json())
+  .then((json) => console.log(json))
+  .catch((e) => console.log(e.message)); */
